7c9394b8f34670814d2f0b1f76ea59475458264f
Francesco Sblendorio <sblendorio@gmail.com>
2023-12-31 11:29:53 +0100

    no find but globbing

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ rm -rf out
 mkdir -p out
 
 cd sbc1
-for f in $(find . -name '*.s'); do
+for f in *.s; do
   base="${f%.*}"
   ca65 -g $f -o ../out/${base}.o -l ../out/${base}.lst --list-bytes 0
   ld65 -o ../out/${base}.bin -Ln ../out/${base}.labels -m ../out/${base}.map -C system.cfg ../out/${base}.o
